0ed520df7b0918693dd9e7b52fab16d2735bb0db
Alexei Kananenka <kananenka@gmail.com>
2018-12-26 17:56:00 -0600

    Update energy.hpp

--- src/energy.hpp
+++ src/energy.hpp
@@ -1,14 +1,15 @@
 #include "util.hpp"
 #include "const.hpp"
+#include "qs.hpp"
 
 #ifndef EN_H
 #define EN_H
 
-void energy(double &ek, double &ep, double &elj,
+void energy(double &ek, double &elj,
             double &ec, double &tempK, double* vel,
             double* xyz, double* mass, int natoms,
             int Nconst, double* vij, double* sigma,
             double *eps, double eps_r, double* box,
-            double r_cut);
+            double r_cut, Subsystem &QS);
 
 #endif
