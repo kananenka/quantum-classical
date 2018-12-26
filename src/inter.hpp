fb419b8e2c470042d4856abf291cf710dff18650
Alexei Kananenka <kananenka@gmail.com>
2018-12-26 16:12:22 -0600

    Update inter.hpp

--- src/inter.hpp
+++ src/inter.hpp
@@ -1,5 +1,6 @@
 #include "const.hpp"
 #include "util.hpp"
+#include "qs.hpp"
 
 #ifndef INT_H
 #define INT_H
@@ -7,9 +8,9 @@
 void coulomb_m(double* charges, double* vij, int natoms,
                bool* inter);
 
-void force_c(double *forces, double *vij, double* sigma, double* eps,
-             double *xyz, double eps_r, double *box, double r_cut, 
-             int natoms);
+void force_c(Subsystem &QS, double *forces, double *vij, double* sigma, 
+             double* eps, double *xyz, double eps_r, double *box, 
+             double r_cut, int natoms);
 
 
 #endif
