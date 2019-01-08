ef80ef95fe9d209117f6bc34bdd0dee3c35e925f
Alexei Kananenka <kananenka@gmail.com>
2019-01-07 19:16:07 -0600

    Update move.hpp

--- src/move.hpp
+++ src/move.hpp
@@ -1,11 +1,15 @@
-#include <ctime>
-#include <cmath>
-#include <complex>
-#include <string>
-#include <chrono>
+#include "qs.hpp"
+#include "inter.hpp"
+#include "util.hpp"
+#include "settle.hpp"
 
 #ifndef MV_H
 #define MV_H
+
+void move(Subsystem &QS, Settle &Stl, double* xyz, double* vel, double* mass,
+          double* vij, double* sigma, double* eps, double eps_r,
+          double* box, double* forces, int natoms, double lj_cut, 
+          double dt, int atoms_mol, int nmols, int alpha);
 
 void com_v(double* mass, double* vel, int natoms);
 
